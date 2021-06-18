5efd2d48ff2456b15ede99681dd35673821d237d
Nicholas Moen <arcanemachine@tutanota.com>
2021-06-18 15:41:20 -0600

    Click QR code to hide it temporarily

--- code.js
+++ code.js
@@ -6,16 +6,30 @@ function generateNewQrCode(el) {
     // generate QR code and insert into DOM element
     new QRCode(document.getElementById(el.id), {
         text: window.location.href,
-        width: 250,
-        height: 250,
+        width: 300,
+        height: 300,
         colorDark : "#000000",
         colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
+        correctLevel : QRCode.CorrectLevel.H,
     });
   } else {
     // generate message if URL is too long
     el.innerHTML = "<h2>URL too long for QR code!</h2>";
   }
+  el.children[1].title = "Click to temporarily hide this QR code";
+}
+
+function temporarilyHideQrCode(el) {
+  el.style.opacity = '0';
+  setTimeout(() => {
+    el.style.display = 'none';
+    setTimeout(() => {
+      el.style.display = '';
+      setTimeout(() => {
+        el.style.opacity = '';
+      }, 500);
+    }, 3000);
+  }, 500);
 }
 
 window.addEventListener('load', (event) => {
@@ -32,10 +46,12 @@ window.addEventListener('load', (event) => {
   qrEl.style.justifyContent = 'center';
   qrEl.style.alignItems = 'center';
 
+  qrEl.style.opacity = '0'; // opacity initially set to 0 to allow fade-in
   qrEl.style.transition = 'opacity 0.5s';
-  qrEl.style.zIndex = '1000';
+  qrEl.style.zIndex = '2147483646';
 
-  // when mouse is hovered over 
+
+  // when mouse is hovered over QR code, fade out to view content below
   let css = `
     #omni-qr-element { opacity: 0.8; }
     #omni-qr-element:hover { opacity: 0.1; }
@@ -48,13 +64,22 @@ window.addEventListener('load', (event) => {
     style.appendChild(document.createTextNode(css));
   }
 
-  document.getElementsByTagName('head')[0].appendChild(style);
-
   // append placeholder element to the end of the body
+  document.getElementsByTagName('head')[0].appendChild(style);
   document.body.appendChild(qrEl);
 
   // generate first QR code
   generateNewQrCode(qrEl);
+
+  // fade in after initial render
+  setTimeout(() => {
+    qrEl.style.opacity = '';
+  });
+
+  // add click listener to temporarily hide the QR code if it is in the way
+  qrEl.addEventListener('click', () => {
+    temporarilyHideQrCode(qrEl);
+  });
 })
 
 // update the QR code when the URL changes
