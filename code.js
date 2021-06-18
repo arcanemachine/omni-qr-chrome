6c651b554b1b77071c063f1b1ef276d4190ea3c7
Nicholas Moen <arcanemachine@tutanota.com>
2021-06-18 12:08:33 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', (event) => {
+
+  // generate QR code placeholder element
+  const qrEl = document.createElement('div');
+  qrEl.id = "omni-qr-element";
+
+  // append placeholder element to the end of the body
+  document.body.appendChild(qrEl);
+
+  function generateNewQrCode() {
+    // clear the element's content
+    qrEl.innerHTML = '';
+
+    // generate QR code and insert into generated DOM element
+    let qrcode = new QRCode(document.getElementById(qrEl.id), {
+        text: window.location.href,
+        width: 250,
+        height: 250,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+  }
+
+  generateNewQrCode();
+
+  // update the qr code when the URL changes
+  setInterval(() => {
+    generateNewQrCode()
+  }, 10000);
+})
